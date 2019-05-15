99b90962f50dc1f52ceaa513dfa35872de57a235
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-15 22:32:14 +0100

    Animated menu and articles.

--- Article/Article.js
+++ Article/Article.js
@@ -8,22 +8,12 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    // this.expandButton.textContent = 'expand';
-    this.articleLabel();
+    this.expandButton.textContent = '-----';
+   
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
-  articleLabel() {
-    const noButtonLabelYet = !this.expandButton.textContent;
-    const labelIsClose = this.expandButton.textContent === 'Close';
-
-    if (noButtonLabelYet || labelIsClose) {
-      this.expandButton.textContent = 'Open';
-    } else {
-      this.expandButton.textContent = 'Close';
-    }
-  }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
@@ -31,7 +21,7 @@ class Article {
     if (this.domElement.style.height === '400px') {
       TweenMax.to(this.domElement, 0.5, { height: 50, ease: Power3.easeOut });
     } else {
-      TweenMax.to(this.domElement, 1, { height: 400, ease: Power1.easeOut });
+      TweenMax.to(this.domElement, 0.5, { height: 400, ease: Power1.easeOut });
     }
   }
 }
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,17 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open');
-};
+  // menu.classList.toggle('menu--open');
 
+  if (menu.style.width === '350px') {
+    TweenMax.to(menu, 0.5, { width: 0, ease: Power1.easeOut });
+  } else {
+    TweenMax.to(menu, 0.5, { width: 350, ease: Power1.easeOut });
+  }
+};
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
